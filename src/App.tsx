18da97227fa086f69cbe99e2e7b86d8b66006ef8
Damiano Plebani <damiano.plebani@pagopa.it>
2022-09-27 14:34:12 +0200

    Add CRA and Ditto-related packages

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { Ditto, DittoProvider } from 'ditto-react';
+import dittoData from './ditto';
+
+const App = () => (
+  <DittoProvider source={dittoData}>
+    <div className="App">
+      <header className="App-header">
+        <ul>
+          <li>
+            <Ditto componentId="app-io.menu.messaggi" />
+            <Ditto componentId="app-io.menu.portafoglio-1" />
+            <Ditto componentId="app-io.menu.profilo" />
+          </li>
+        </ul>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  </DittoProvider>
+);
+
+export default App;
